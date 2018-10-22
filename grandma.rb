93569aac1380d001dd920d337252bb1ef99e5606
katejsenger <katejsenger@yahoo.com>
2018-10-22 19:15:38 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 def speak_to_grandma(phrase)
-  if phrase == .upcase
+  if phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  else phrase == "Hello, grandma".downcase
+  else phrase == phrase.downcase
     return "HUH?! SPEAK UP, SONNY!"
   end 
 end
