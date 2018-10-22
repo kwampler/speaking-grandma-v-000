78e2f5ebb2f939e995f91d2a7ed7578bfc5cf02a
katejsenger <katejsenger@yahoo.com>
2018-10-22 19:23:39 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -5,6 +5,8 @@ def speak_to_grandma(phrase)
       return "NO, NOT SINCE 1938!"
     elsif phrase == phrase.downcase
       return "HUH?! SPEAK UP, SONNY!"
+    else
+      return "HUH?! SPEAK UP, SONNY!"
   end 
 end
 
